68a28263b14e6397582e8124f0a82b0f4baf91f4
jesari-git <158497071+jesari-git@users.noreply.github.com>
2025-07-16 12:15:53 +0200

    CAN, yaml corrected

--- src/peripheral.v
+++ src/peripheral.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tqvp_full_CAN (
+module tt_um_tqv_jesari_CAN (
     input         clk,          // Clock - the TinyQV project clock is normally set to 64MHz.
     input         rst_n,        // Reset_n - low to reset.
 
